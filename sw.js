3613703f3c9c19527800c68e212e5cb9d6842784
Marina Sanchez <masaro1411@gmail.com>
2022-10-22 00:34:06 -0500

    Manejo de errores en el Fecth Event

--- sw.js
+++ sw.js
@@ -3,10 +3,13 @@
 
 self.addEventListener('fetch', event => {
 
-    if( event.request.url.includes( 'main.jpg') ) {
-        
+    const resp = fetch( event.request )
+        .then( resp => {
 
-        event.respondWith( fetch( 'img(main-patas-arriba.jpg') );
+            return resp.ok ? resp ? fetch('img/main.jpg');
+        });
+
+        event.respondWith(resp);
          
     }
 })
